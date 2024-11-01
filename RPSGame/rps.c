330d850601fb9dea820e020d548b4325afd0bb54
Ishaq Ishaq Nasiru <Inasiru3540@conestogac.on.ca>
2024-11-01 00:56:04 -0400

    Starting production code

--- RPSGame/rps.c
+++ RPSGame/rps.c
@@ -1,6 +1,19 @@
+#define _CRT_SECURE_NO_WARNINGS
+
+#include <string.h>
+#include "rps.h"
+
 
 //function implementation
+void rpsgame(const char* playerone, const char* playertwo, char* result) 
+{
+    char p1[20];
 
-void rpsgame(const char* playerone, const char* playertwo, char* result) {
+    char p2[20];
+    strncpy(p1, playerone, sizeof(p1) - 1);
+
+    strncpy(p2, playertwo, sizeof(p2) - 1);
+
+   
 
 }
--- UnitTest/UnitTest.cpp
+++ UnitTest/UnitTest.cpp
@@ -1,9 +1,26 @@
+#define _CRT_SECURE_NO_WARNINGS
 #include "pch.h"
 #include "CppUnitTest.h"
+
 
 using namespace Microsoft::VisualStudio::CppUnitTestFramework;
 
 void rpsgame(const char* playerone, const char* playertwo, char* result);
+
+void rpsgame(const char* playerone, const char* playertwo, char* result) {
+	char p1[20];
+
+	char p2[20];
+
+	strncpy(p1, playerone, sizeof(p1) - 1);
+
+	strncpy(p2, playertwo, sizeof(p2) - 1);
+
+	
+
+	
+
+}
 
 namespace UnitTest
 {
@@ -25,7 +42,7 @@ namespace UnitTest
 		{
 			char result[20];
 			rpsgame("Scissors", "Rock", result);
-			Assert::AreEqual("Player2", result);
+			Assert::AreEqual("Playertwo", result);
 
 		}
 		TEST_METHOD(WhenThereisADraw)
@@ -47,7 +64,7 @@ namespace UnitTest
 		{
 			char result[20];
 			rpsgame("Rock", "Scissors", result);
-			Assert::AreEqual("Player1", result);
+			Assert::AreEqual("Playerone", result);
 		}
 
 	};
